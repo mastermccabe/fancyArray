1e921f0c7546ab0e69f4f0510e09ca6bf0d75872
McCabe <mccabe@wambl.com>
2017-08-21 15:16:45 -0700

    updated for extra credits

--- fancyArray.js
+++ fancyArray.js
@@ -1,6 +1,18 @@
-function fancyArray() {
+function fancyArray(sText, isReversed) {
+  sText = sText || '-->';
+
+
   var arr = ["James", "Jill", "Jane", "Jack"];
-  for (var i = 0; i < arr.length; i++) {
-    console.log(i + "->" + arr[i]);
+  if (isReversed == 'false') {
+    for (var i = 0; i < arr.length; i++) {
+      console.log(i + sText + arr[i]);
+    }
+  } else if (isReversed == 'true') {
+    for (var i = arr.length - 1; i >= 0; i--) {
+      console.log(i + sText + arr[i]);
+    }
   }
+
 }
+fancyArray('-----!!!!--->', 'true');
+fancyArray('-****----->', 'false');
